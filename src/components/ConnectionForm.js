4447e7be5f8addf6b0e50f51d83ad6ec06d4af8f
America Mendoza <america@localhost.localdomain>
2021-02-25 13:04:32 -0800

    form select and text area added

--- src/components/ConnectionForm.js
+++ src/components/ConnectionForm.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Form from "react-bootstrap/Form";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Topic from "./Topic";
 import NPIf from "np-if";
 import NPElse from "np-if/src/NPElse";
 import NPInput from "./NPInput";
@@ -32,16 +30,36 @@ class ConnectionForm extends React.Component{
                     </div>
                     <FontAwesomeIcon className={'icon-link'} icon={'link'}/>
                 </div>
-                <div className={'sidebar-form'}>
+                <div className={'sidebar-form mt-3'}>
                     <label>1. Select the citizen’s contributions on the left side. </label>
-                    <NPInput label={'If not on the list, add a new topic:  '}/>
+
+                    {/*select */}
+                    <label className={'mt-2'}>2. To which research topic can the contribution(s) be linked to? (*)  </label>
+                    <select>
+                        <option>Select a topic </option>
+                    </select>
+
+                    <NPInput wrapperClass={'mt-2'} label={'If not on the list, add a new topic:  '}/>
+
+                    {/*select */}
+                    <label className={'mt-2'}>3. From which perspective are you making this connection? (from a discipline or your position) (*)   </label>
+                    <select>
+                        <option>Select a perspective </option>
+                    </select>
 
                     <NPInput label={'If not on the list, add a new perspective: '}/>
-                    <NPInput label={'4. Please explain the connection of the contributions to the topic'}
+
+                    <NPInput wrapperClass={'mt-2'}
+                             type={'text-area'}
+                             label={'4. Please explain the connection of the contributions to the topic'}
                              placeholder={'This contribution is related ... '}/>
-                    <NPInput label={'5. Can you add some links to research resources? (optional)'}
+                    <NPInput wrapperClass={'mt-2'}
+                             type={'text-area'}
+                             label={'5. Can you add some links to research resources? (optional)'}
                              placeholder={'Research about AI (www.researchgate.net/789890) '}/>
-                    <NPInput label={'6. Can you propose topics for new ministry funding calls? (optional)'}
+                    <NPInput wrapperClass={'mt-2'}
+                             type={'text-area'}
+                             label={'6. Can you propose topics for new ministry funding calls? (optional)'}
                              placeholder={'A topic to the ministry comprises ... '}/>
 
 
