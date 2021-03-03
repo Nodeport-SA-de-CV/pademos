73d612e618531e6d49af91f35826b652f4c968e2
America Mendoza <america@localhost.localdomain>
2021-03-02 19:46:39 -0800

    contribution style

--- src/components/plots/Contribution.js
+++ src/components/plots/Contribution.js
@@ -1,5 +1,8 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Icon1 from '../../img/icon1.png';
+import Icon2 from '../../img/icon2.png';
 class Contribution extends React.Component {
 
     constructor(props) {
@@ -12,7 +15,12 @@ class Contribution extends React.Component {
     render(){
         return(
             <div className={`contribution ${this.props.index}`} style={{backgroundColor:this.props.bgColor}}>
-                {this.props.index} - {this.props.contribution.document_what}
+                <div className={'c-title mb-auto mt-auto'}>{this.props.contribution.document_what}</div>
+                <div className={'c-wrapper-icons'}>
+                    <img className={'c-icon'} src={Icon1}/>
+                    <img className={'c-icon'} src={Icon2}/>
+
+                </div>
             </div>
         )
     }
