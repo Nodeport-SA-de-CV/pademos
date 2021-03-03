26adb29f4b3beec6b6d1c1f0e79b5bf8b082c8b6
America Mendoza <america@localhost.localdomain>
2021-03-02 18:31:53 -0800

    group style

--- src/components/plots/Group.js
+++ src/components/plots/Group.js
@@ -1,27 +1,49 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import Contribution from "./Contribution";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
 class Group extends React.Component {
 
     constructor(props) {
         super(props);
         this.state     = {}
+        this.onClickShowKeywords = this.onClickShowKeywords.bind(this);
     }
 
     componentDidMount() {
     }
+
+    onClickShowKeywords(){
+        console.log('clicked v');
+    }
+
     render(){
-        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+        var c = Math.floor(Math.random()*16777215).toString(16);
+        const color = `#${c}`;
         return(
-            <div style={{backgroundColor:"#" +randomColor}}>
-                {this.props.title}
+            <div>
+                <div className={'group-title'} style={{color:color}}>
+                    <div className={'mr-auto'}>{this.props.title}</div>
+                    <div className={'group-keywords'}>
+                         <div>#keyword 1</div>
+                         <div>#keyword 2</div>
+                    </div>
+                    <FontAwesomeIcon className={'group-icon'}
+                                     onClick={() => this.onClickShowKeywords()}
+                                     icon={'angle-down'}/>
+                </div>
                 <div className={'group-container'} >
                     {
                         this.props.group.map((contribution,index   ) =>{
                             return(
-                                <Contribution contribution={contribution} key={index} index={index}>
-
-                                </Contribution>
+                                <Contribution contribution={contribution}
+                                              key={index}
+                                              index={index}
+                                              bgColor={color}
+                                />
                             )
 
                         })
