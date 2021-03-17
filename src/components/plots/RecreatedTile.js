04f5ba60c118a39ce6b0f05a492e3f41802cb561
America Mendoza <america@localhost.localdomain>
2021-03-16 20:47:15 -0700

    contribution added

--- src/components/plots/RecreatedTile.js
+++ src/components/plots/RecreatedTile.js
@@ -1,18 +1,54 @@
 import React from "react";
 import PropTypes from "prop-types";
+import NPIf from "np-if";
+import API from "../../lib/api/API";
+import UISelector from "./ui/UISelector";
 
 class RecreatedTile extends React.Component {
     render(){
+        const styleTile = {
+            width:this.props.width,
+            height:this.props.height,
+            left: this.props.left,
+            top:this.props.top,
+            backgroundColor: this.props.contribution.isDisabled ? 'gray' : this.props.color
+        }
+        const keywords = this.props.contribution.document_keywords ? this.props.contribution.document_keywords : [];
+
         return (
-            <div style={{
-                position:"absolute",
-                width:this.props.width,
-                height:this.props.height,
-                left: this.props.left,
-                top:this.props.top,
-                backgroundColor: this.props.contribution.isDisabled ? 'gray' : this.props.color
-            }} className={'recreated-tile'}>
-               
+            <div className={'recreated-tile'} style={styleTile}>
+                <div className={'rt-header'}>
+                    <UISelector isSelected={this.props.isSelected}/>
+                    <div className={'rt-title'}>{this.props.contribution.document_title}</div>
+                </div>
+
+                <div className={'rt-content'}>
+                    <div className={'rt-keywords'}>
+                        {
+                            keywords.map((keyword,index) => {
+                                return(
+                                    <div key={index}>{keyword}</div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className={'rt-description'}>
+                        {this.props.contribution.document_what}
+                    </div>
+                    <div className={'rt-show-all-btn'}
+                         onClick={(c) => this.onClickShowDetails()}>... alles anzeigen
+                    </div>
+                </div>
+
+                <div className={'rt-footer'}>
+                    {
+                        this.props.contribution.icons.map((i,index) =>{
+                            return(
+                                <img key={index} className={'rt-icon'} src={`${API.API_URL}/icons/${i}`} />
+                            )
+                        })
+                    }
+                </div>
             </div>
         )
     }
@@ -22,7 +58,9 @@ RecreatedTile.propTypes = {
     height: PropTypes.number,
     left: PropTypes.number,
     right: PropTypes.number,
-    contribution: PropTypes.object
+    contribution: PropTypes.object,
+    isSelected: PropTypes.bool,
+    onClickContributionDetails: PropTypes.func
 };
 
 RecreatedTile.defaultProps = {
@@ -30,7 +68,9 @@ RecreatedTile.defaultProps = {
     height: 0,
     left: 0,
     top: 0,
-    contribution:{}
+    contribution:{},
+    isSelected: true,
+    onClickContributionDetails: () => {}
 };
 export default RecreatedTile;
 
