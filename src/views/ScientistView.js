9e355fc0504bca4bed640a3fadea27322945a1ad
America Mendoza <america@localhost.localdomain>
2021-04-12 19:49:34 -0700

    check and footer added

--- src/views/ScientistView.js
+++ src/views/ScientistView.js
@@ -8,6 +8,7 @@ import PlotView from "./plots/PlotView";
 import GroupList from "../components/plots/GroupList";
 import API from "../lib/api/API";
 import SidebarScientist from "../components/SidebarScientist";
+import Form from "react-bootstrap/Form";
 const _ = require('underscore');
 
 class ScientistView extends React.Component {
@@ -48,7 +49,7 @@ class ScientistView extends React.Component {
                             />
                             <div className={'header-row align-items-start pt-3 pb-3'}>
                                 Übersicht
-                                <div className={'header-select-wrapper ml-4'}>
+                                <div className={'header-select-wrapper ml-4'} style={{fontSize:'0.8rem'}}>
                                     <label>Geöffnete Themen mit zugeordneten Perspektiven:  </label>
                                     <select style={{height: '37.2px'}}
                                             onChange={(e) => this.onChange(e.target.value)}>
@@ -56,7 +57,7 @@ class ScientistView extends React.Component {
                                     </select>
                                 </div>
                             </div>
-
+                            <div className={'d-flex mb-auto'}></div>
                             {/*<PlotView ref={(ref) => this.plotView = ref}*/}
                             {/*          selectedTopic={this.state.selectedTopic}*/}
                             {/*          onTopicsLoaded={(topics) => this.onTopicsLoaded(topics)}*/}
@@ -68,6 +69,27 @@ class ScientistView extends React.Component {
                             {/*          }}*/}
                             {/*          onShowContributionsDetails={(show) => this.setState({isActionsDisabled: show})}*/}
                             {/*/>*/}
+
+                            <div className={'header-row align-items-center pt-3 pb-3'} style={{fontSize:'0.8rem'}}>
+                                <strong className={'mr-auto'}>Verbindungen markieren, die</strong>
+                                <Form className={'mr-auto'} >
+                                    <Form.Check
+                                                id="links"
+                                                type="checkbox"
+                                                label="Links zu Forschungsressourcen" />
+                                </Form>
+                                <Form>
+                                    <Form.Check
+                                                id="financing"
+                                                type="checkbox"
+                                                label="Finanzierungsvorschläge enthalten" />
+                                </Form>
+                            </div>
+                            <div className={'header-row align-items-start justify-content-end pt-3 pb-3'} style={{fontSize:'0.8rem'}}>
+                                Um eine neue Verbindung anzulegen, wechseln Sie bitte in die Ansicht "Bürger:innen".<br/>
+                                Dort können Sie die zu verbindenden Beiträge auswählen und markieren.
+                            </div>
+
                         </div>
                         <SidebarScientist/>
                     </div>
