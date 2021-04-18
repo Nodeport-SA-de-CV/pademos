c31d6f67130a3db68566ddd9fc8d1a204f6881d6
America Mendoza <america@localhost.localdomain>
2021-04-18 13:59:03 -0700

    icon added

--- src/components/scientist/MapWrapper.js
+++ src/components/scientist/MapWrapper.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import API from "../../lib/api/API";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
 const _ = require('underscore');
@@ -13,12 +15,27 @@ class MapWrapper extends React.Component {
     render() {
         const color = this.props.color;
         const style = {
-            backgroundColor: this.props.color
+            backgroundColor: color
         }
         const data = this.props.data;
         return (
             <div className={'h-100 d-flex map-wrapper'} style={style}>
-                <div className={'mw-header'}>{data.topic}</div>
+                <FontAwesomeIcon className={'mw-icon'} size={'lg'}
+                                 icon={'times'}
+                                 onClick={() => this.props.onClickClose()}/>
+                <div className={'mw-header mb-3'}>
+                    <img className={'mr-3'}
+                         src={`${API.API_URL}/icons/${data.icon}`}
+                         height={36}/>
+                    <div>
+                        <div className={'title'}>
+                            {data.topic}
+                        </div>
+                        <div>
+                            Ein Forschungsthema kann mehrere Perspektiven haben, die von Wissenschaftler:innen angelegt wurden. Bitte wählen Sie eine:
+                        </div>
+                    </div>
+                </div>
                 {this.props.children}
             </div>
         );
@@ -28,10 +45,11 @@ export  default MapWrapper;
 MapWrapper.propTypes = {
     data                  : PropTypes.object,
     color                 : PropTypes.string,
+    onClickClose          : PropTypes.func
 };
 
 MapWrapper.defaultProps = {
     data                  : {},
     color                 : '',
-
+    onClickClose          : () => {}
 };
